f01f8c850e5a033ff022459e5777bde5e3db3381
vicks <vicks@vicks-desktop.lan>
2010-11-02 23:37:09 +0000

    First commit

---
+++ src/org/example/sudoku/Sudoku.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.example.sudoku;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *
+ * @author vicks
+ */
+public class Sudoku extends Activity {
+
+    /** Called when the activity is first created. */
+    @OverrideBLAHLBLALG
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        // ToDo add your GUI initialization code here        
+    }
+
+}
